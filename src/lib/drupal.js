d0d47383e1d2aee4054f30cb0f5f6eb6b54f7d30
Greyfrut <greyfrut@wishdesk.com>
2024-03-28 11:30:50 +0200

    dasd

--- src/lib/drupal.js
+++ src/lib/drupal.js
@@ -1,8 +1,8 @@
-const BASE_URL = 'http://drupal-vnu.docksal.site'; // Замініть на URL вашого сайту Drupal
+const BASE_URL = 'https://back.vnu.ms1-wishdesk.com'; // Замініть на URL вашого сайту Drupal
 
 export const getAllArticles = async () => {
   console.log('testsete')
-  const res = await fetch(`${BASE_URL}/jsonapi/node/article`);
+  const res = await fetch(`${BASE_URL}/jsonapi/node/news`);
   console.log(res)
   
   if (!res.ok) {
@@ -13,8 +13,8 @@ export const getAllArticles = async () => {
   return data.data;
 };
 export const getArticleById = async (id) => {
-  console.log(`${BASE_URL}/jsonapi/node/article/${id}`);
-  const res = await fetch(`${BASE_URL}/jsonapi/node/article/${id}`);
+  console.log(`${BASE_URL}/jsonapi/node/news/${id}`);
+  const res = await fetch(`${BASE_URL}/jsonapi/node/news/${id}`);
   if (!res.ok) {
     throw new Error(`Failed to fetch article ${id}: ${res.status} ${res.statusText}`);
   }
